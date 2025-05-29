b69b1228a1317e8eb84c5e546ae67c0a70988ac1
sarenaud <32renaud.simon@gmail.com>
2025-05-29 13:01:10 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -25,27 +25,12 @@ let Car = sprites.create(img`
 Car.follow(mySprite)
 mySprite.z = 0
 Car.z = 1
-tiles.placeOnTile(Render.getRenderSpriteInstance(), tiles.getTileLocation(2, 15))
-tiles.placeOnTile(Car, tiles.getTileLocation(2, 15))
-fakeButtons.setPlayerConnected(controller.player2, true)
-let mySprite2 = sprites.create(img`
-    . . . . . . . . c c c c . . . . 
-    . . . . . . c c d d d d c . . . 
-    . . . . . c c c c c c d c . . . 
-    . . . . c c 4 4 4 4 d c c . c c 
-    . . . c 4 d 4 4 4 4 4 1 c c 4 c 
-    . . c 4 4 4 1 4 4 4 4 d 1 c 4 f 
-    . c 4 4 4 4 1 4 4 4 4 4 1 4 4 f 
-    f 4 4 4 4 4 1 1 c f 4 4 1 f 4 f 
-    f 4 4 4 f 4 1 c 4 f 4 4 1 f 4 f 
-    f 4 4 4 4 4 1 4 4 f 4 4 d f f f 
-    . f 4 4 4 4 1 c c 4 4 d f f . . 
-    . . f f 4 d 4 4 4 4 4 c f . . . 
-    . . . . f f 4 4 4 4 c d b c . . 
-    . . . . . . f f f f d d d c . . 
-    . . . . . . . . . . c c c . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
+tiles.placeOnTile(Car, tiles.getTileLocation(2, 13))
 tiles.placeOnTile(mySprite, tiles.getTileLocation(2, 13))
+fakeButtons.setPlayerConnected(controller.player2, true)
 controller.player2.moveSprite(mySprite, 55, 50)
 fakeButtons.pressMultiplayerButton(controller.player2, ControllerButton.Right)
+let time = 0
+game.onUpdate(function () {
+    Car.sayText(game.runtime() / 1000)
+})
